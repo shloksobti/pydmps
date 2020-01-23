fbe436a437dbf9b2c1e26f3b1e4f83c0ee8d8a88
shloksobti <shlok.sobti@gmail.com>
2020-01-23 07:56:35 -0600

    fixes

--- pydmps/dmp.py
+++ pydmps/dmp.py
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import numpy as np
 
-from pydmps.cs import CanonicalSystem
+from cs import CanonicalSystem
 
 
 class DMPs(object):
